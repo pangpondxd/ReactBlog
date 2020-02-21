8bd1c2539fca7e98d631874aca80c4ef76492e8a
SirawichDev <sirawit0676@gmail.com>
2020-02-22 00:38:34 +0700

    fixed import image path problem and fixed #2 problem �

--- src/Component/FooterButton.js
+++ src/Component/FooterButton.js
@@ -1,103 +1,105 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import imageStatic from '../static/Garden.jpg'
-import { red } from '@material-ui/core/colors';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import imageStatic from "./static/Garden.jpg";
+import { red } from "@material-ui/core/colors";
 const images = [
   {
-    url: './static/1.jpg',
-    title: 'Blog',
-    width: '40%',
-    height: '100%',
-    href: '/blog'
+    url: "./static/1.jpg",
+    title: "Blog",
+    width: "40%",
+    height: "100%",
+    href: "/blog"
   },
   {
-    url: './static/2.jpg',
-    title: 'Coming Soon 1 ',
-    width: '30%',
-    href: '#'
+    url: "./static/Garden.jpg",
+    title: "Coming Soon 1 ",
+    width: "30%",
+    href: "#"
   },
   {
-    url: '',
-    title: 'Coming Soon 2',
-    width: '30%',
-    href: '#'
-  },
+    url: "./static/Blog.jpg",
+    title: "Coming Soon 2",
+    width: "30%",
+    href: "#"
+  }
 ];
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
     minWidth: 300,
-    width: '100%',
+    width: "100%"
   },
   image: {
-    position: 'relative',
+    position: "relative",
     height: 720,
-    [theme.breakpoints.down('xs')]: {
-      width: '100% !important', // Overrides inline-style
-      height: 100,
+    overflow: "hidden",
+    [theme.breakpoints.down("xs")]: {
+      width: "100% !important", // Overrides inline-style
+      height: 100
     },
-    '&:hover, &$focusVisible': {
+    "&:hover, &$focusVisible": {
       zIndex: 1,
-      '& $imageBackdrop': {
-        opacity: 0.15,
+      "& $imageBackdrop": {
+        opacity: 0.15
       },
-      '& $imageMarked': {
-        opacity: 0,
+      "& $imageMarked": {
+        opacity: 0
       },
-      '& $imageTitle': {
-        border: '4px solid currentColor',
-      },
-    },
+      "& $imageTitle": {
+        border: "4px solid currentColor"
+      }
+    }
   },
   focusVisible: {},
   imageButton: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
     top: 0,
     bottom: 0,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: theme.palette.common.white,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: theme.palette.common.white
   },
   imageSrc: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
     top: 0,
     bottom: 0,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center 40%',
+    backgroundSize: "cover",
+    backgroundPosition: "center 40%"
   },
   imageBackdrop: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
     top: 0,
     bottom: 0,
     backgroundColor: theme.palette.common.black,
     opacity: 0.4,
-    transition: theme.transitions.create('opacity'),
+    transition: theme.transitions.create("opacity")
   },
   imageTitle: {
-    position: 'relative',
-    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) + 6}px`,
+    position: "relative",
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) +
+      6}px`
   },
   imageMarked: {
     height: 3,
     width: 18,
     backgroundColor: theme.palette.common.white,
-    position: 'absolute',
+    position: "absolute",
     bottom: -2,
-    left: 'calc(50% - 9px)',
-    transition: theme.transitions.create('opacity'),
-  },
+    left: "calc(50% - 9px)",
+    transition: theme.transitions.create("opacity")
+  }
 }));
 
 export default function ButtonBases() {
@@ -113,19 +115,12 @@ export default function ButtonBases() {
           href={image.href}
           focusVisibleClassName={classes.focusVisible}
           style={{
-            width: image.width,
+            width: image.width
           }}
         >
-          <span
-            className={classes.imageSrc}
-            style={{
-              // backgroundImage : `url(${image.url})`,
-              backgroundColor: 'pink',
-              
-            }}
-            
-          />
-          <h1>{image.url}</h1>
+          <span className={classes.imageSrc}>
+            <img className={classes.image} src={require(`${image.url}`)} />
+          </span>
           <span className={classes.imageBackdrop} />
           <span className={classes.imageButton}>
             <Typography
